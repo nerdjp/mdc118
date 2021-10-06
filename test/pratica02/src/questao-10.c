d289fc9b9ee4454093ed570a1420faa95deb40cc
nerdjp <joaopedrofigm@gmail.com>
2021-10-06 10:26:41 -0300

    questão 10 melhorada

--- test/pratica02/src/questao-10.c
+++ test/pratica02/src/questao-10.c
@@ -5,8 +5,12 @@ int main()
 	int n;
 	printf("Insira o número desejado: ");
 	scanf("%d", &n);
+	printf("\n");
 
-	printf( "\n %d x 1 = %d\n %d x 2 = %d\n %d x 3 = %d\n %d x 4 = %d\n %d x 5 = %d\n %d x 6 = %d\n %d x 7 = %d\n %d x 8 = %d\n %d x 9 = %d\n %d x 10 = %d\n", n, n, n, n * 2, n, n * 3, n, n * 4, n, n * 5, n, n * 6, n, n * 7, n, n * 8, n, n * 9, n, n * 10 );
+	for(int i = 1; i <= 10; i++)
+	{
+		printf("%d x %d = %d\n", n, i, n * i);
+	}
 
 	return 0;
 }
